02b27660525774f70de9cd242aea28035a142f71
Yassine KHLOUFI <ykhloufi@jehann.fr>
2025-07-21 16:37:05 +0200

    Création de la page about et test request et param en url

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,8 +11,26 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return  $this->render('main/index.html.twig', [
+        return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
+    }
+
+    #[Route('/a-propos', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('main/about.html.twig');
+    }
+
+    #[Route('/ticket/{name}', name: 'app_ticket', defaults: ['name' => 'ticket'])]
+    public function ticket(string $name): Response
+    {
+        return new Response('Votre ticket d\'aide : ' . $name . ' !');
+    }
+
+    #[Route('/book/{id}', name: 'app_book', requirements: ['id' => '\d+'])]
+    public function book(int $id): Response
+    {
+        return new Response('Votre livre a pour identifiant : ' . $id . ' !');
     }
 }
