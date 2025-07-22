7a360647208cc4eaae400164d46f5f228b79f855
Yassine KHLOUFI <ykhloufi@jehann.fr>
2025-07-22 14:05:07 +0200

    Ajout d'une méthode update au Controller Etablissement

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Etablissement;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,5 +25,23 @@ class EtablissementController extends AbstractController
         $em->flush();
 
         return new Response('Entité  avec ID ' . $etablissement->getId() . ' a été enregistrée avec succes');
+    }
+
+    #[Route('/update', name: 'app_etablissement_update')]
+    public function update(EntityManagerInterface $em): Response
+    {
+        $id = 14; // ID à modifier
+
+        // Récupération de l'entité via EntityManager
+        $etablissement = $em->getRepository(Etablissement::class)->find($id);
+
+        if(!$etablissement){
+            throw $this->createNotFoundException("Etablissement n'existe pas");
+        }
+
+        $etablissement->setDescription('Description modifiée');
+        $em->flush();
+
+        return new Response("Etablissement #{$id} mis à jour");
     }
 }
