20e0707cf657f1cf5cd54bbd48b915a52578b1c0
Yassine KHLOUFI <ykhloufi@jehann.fr>
2025-07-22 15:59:51 +0200

    Intégration du bundle de Pagination dans le template Twig

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -6,21 +6,39 @@ namespace App\Controller;
 use App\Entity\Etablissement;
 use App\Repository\EtablissementRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EtablissementController extends AbstractController
 {
     #[Route('/etablissements', name: 'app_etablissement_index')]
-    public function index(EtablissementRepository $etablissementRepository): Response
-    {
-        $allEtablissements = $etablissementRepository->createQueryBuilder('e')
-            ->orderBy('e.nom', 'ASC')
-            ->getQuery()->getResult();
+    public function index(
+        EtablissementRepository $etablissementRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $query = $etablissementRepository->createQueryBuilder('e')
+            ->orderBy('e.nom', 'ASC');
+            //->getQuery()->getResult();
+
+        $etablissements = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        //dd($etablissements); // debug pour voir les données paginées reçues par le template
+
+        // plan d'action :
+        // on va installer le bundle de pagination (x)
+        // implémenter dans le controller le service de pagination 'paginate'
+        // mettre à jour le template Twig etablissement/index.html.twig
 
         return $this->render('etablissement/index.html.twig', [
-            'etablissements' => $allEtablissements,
+            'etablissements' => $etablissements,
         ]);
     }
 
