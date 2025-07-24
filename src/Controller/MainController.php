bd9cdb090759ffc0bc8ac0166e7b773e7e04ccf6
Yassine KHLOUFI <ykhloufi@jehann.fr>
2025-07-24 15:11:17 +0200

    feat(i18n): Use LocaleSubscriber for persistent locale

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,6 +21,14 @@ class MainController extends AbstractController
     public function about(): Response
     {
         return $this->render('main/about.html.twig');
+    }
+
+    #[Route('/change-locale/{locale}', name: 'app_change_locale')]
+    public function changeLocale(string $locale, Request $request): Response
+    {
+        $request->getSession()->set('_locale', $locale);
+
+        return $this->redirect($request->headers->get('referer'));
     }
 
     #[Route('/ticket/{name}', name: 'app_ticket', defaults: ['name' => 'ticket'])]
