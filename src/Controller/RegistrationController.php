518bbf597d3360da31c633bb7529b91140178beb
Yassine KHLOUFI <ykhloufi@jehann.fr>
2025-07-25 14:15:11 +0200

    feat(security): Added a Resend Verification Email link and add Methods in Registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,19 +9,26 @@ use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class RegistrationController extends AbstractController
 {
-    public function __construct(private EmailVerifier $emailVerifier)
-    {
-    }
+
+    public function __construct(
+        private EmailVerifier $emailVerifier,
+        private VerifyEmailHelperInterface $verifyEmailHelper,
+        private MailerInterface $mailer,
+    ){}
 
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
@@ -88,5 +95,60 @@ class RegistrationController extends AbstractController
         $this->addFlash('success', 'Votre adresse e-mail a été vérifiée. Vous pouvez maintenant vous connecter.');
 
         return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/verify/resend', name: 'app_resend_verify_email')]
+    public function ResendVerifyEmail(Request $request, UserRepository $userRepository): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre adresse e-mail',
+                    ]),
+                ],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = $form['email']->getData();
+            $user = $userRepository->findOneBy(['email' => $email]);
+
+            if($user && !$user->isVerified()){
+                $this->sendVerificationEmail($user);
+            }
+
+            $this->addFlash('success', 'Si cette adresse e-mail est associé à un compte non vérifié, un nouveau lien de confirmation a été envoyé');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/resend_verify_email.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    private function sendVerificationEmail(User $user): void
+    {
+        $signatureComponents = $this->verifyEmailHelper->generateSignature(
+            'app_verify_email',
+            (string) $user->getId(),
+            (string) $user->getEmail(),
+            ['id' => $user->getId()]
+        );
+
+        $email = (new TemplatedEmail())
+            ->from(new Address('noreply@test.dev', 'Portail CSIESR'))
+            ->to($user->getEmail())
+            ->subject('Merci de confirmer votre adresse e-mail : nouveau lien de vérification')
+            ->htmlTemplate('registration/confirmation_email.html.twig')
+            ->context([
+                'signedUrl' => $signatureComponents->getSignedUrl(),
+                'expiresAtMessageKey' => $signatureComponents->getExpirationMessageKey(),
+                'expiresAtMessageData' => $signatureComponents->getExpirationMessageData(),
+            ]);
+
+        $this->mailer->send($email);
     }
 }
